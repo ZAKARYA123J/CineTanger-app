c383a3da4761dae0dc28287a971d987110389cd9
youssef lab20-u <yousseflab20@gmail.com>
2025-12-24 10:00:04 +0100

    configiration docker

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,10 @@
-import express,{type Request,type Response} from "express"
-const app =express()
-app.get('/',(_req:Request,res:Response)=>{
+import express, { type Request, type Response } from "express"
+const app = express()
+app.get('/', (_req: Request, res: Response) => {
     res.send("hello word")
 })
 
-const PORT=3000
-app.listen(PORT,()=>{
+const PORT = 3000
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
